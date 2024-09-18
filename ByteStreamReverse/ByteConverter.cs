d32309fbd66c7c9f03a00253350c5d8857115a24
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-09-18 17:02:21 +0300

    ready task

---
+++ ByteStreamReverse/ByteConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace ByteStreamReverse;
+
+public class ByteConverter
+{
+    public static string Reverse(int[] bytes)
+    {
+        var result = new StringBuilder();
+
+        for (int i = bytes.Length - 8; i >= 0; i -= 8)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                result.Append(bytes[i + j]);
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ ByteStreamReverse/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ByteStreamReverse;
+
+public class Program()
+{
+    public static void Main()
+    {
+        int[] array = { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0 };
+
+        Console.WriteLine($"Исходный массив: {string.Join(string.Empty, array)}");
+
+        var finalArray = ByteConverter.Reverse(array);
+
+        Console.WriteLine($"Итоговый массив: {finalArray}"); 
+    }
+}
